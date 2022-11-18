99996ebd9d486e9b63aeac0f211087132e7b6cf7
SampooS <sampoos@metropolia.fi>
2022-11-18 10:06:50 +0200

    added some nice and family friendly lyrics

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My balls in your jaw</h1>
+
+      <p>lemme put my balls in yo jaw, my balls in yo jaw.</p>
     </div>
   );
 }
